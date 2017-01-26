4be83f27c85b33347a5b30e3f7f824ba67f029d1
Suziewan <angielinton@me.com>
2017-01-26 18:23:56 -0500

    Initial Commit

---
+++ SimpleTable/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SimpleTable
+//
+//  Created by Angie Linton on 2017-01-26.
+//  Copyright © 2017 Angie Linton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SimpleTable/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SimpleTable
+//
+//  Created by Angie Linton on 2017-01-26.
+//  Copyright © 2017 Angie Linton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
